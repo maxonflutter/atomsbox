e233a016ad9e38c0335acabf1b43520e378fc059
Massimo Del Pezzo <massimo.dpezzo@gmail.com>
2023-04-04 21:03:53 +0700

    Update to breakpoints

--- packages/atomsbox/lib/src/simple/atoms/config/simple_app_breakpoints.dart
+++ packages/atomsbox/lib/src/simple/atoms/config/simple_app_breakpoints.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 class SimpleAppBreakpoints {
-  static bool isNarrow(BuildContext context) =>
-      MediaQuery.of(context).size.width < 1295;
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width <= 600;
 
-  static bool isWide(BuildContext context) =>
-      MediaQuery.of(context).size.width < 1295;
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width < 1295 &&
+      MediaQuery.of(context).size.width > 600;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1295;
 
   // static bool isMobile(BuildContext context) =>
   //     MediaQuery.of(context).size.width < 690;
