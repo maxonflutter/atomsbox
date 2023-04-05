1433dc935b89a03c0de08f08d387db438f41a2ce
Massimo Del Pezzo <massimo.dpezzo@gmail.com>
2023-04-05 17:36:48 +0700

    Pre cache?

--- packages/atomsbox/lib/src/simple/atoms/simple_image.dart
+++ packages/atomsbox/lib/src/simple/atoms/simple_image.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'config/simple_constants.dart';
 
-class SimpleImage extends StatelessWidget {
+class SimpleImage extends StatefulWidget {
   SimpleImage(
     String imageUrl, {
     super.key,
@@ -52,34 +52,99 @@ class SimpleImage extends StatelessWidget {
   final Widget? child;
 
   @override
+  State<SimpleImage> createState() => _SimpleImageState();
+}
+
+class _SimpleImageState extends State<SimpleImage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    precacheImage(widget.image.image, context);
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(borderRadius),
-      child: blurred
+      borderRadius: BorderRadius.circular(widget.borderRadius),
+      child: widget.blurred
           ? ImageFiltered(
               imageFilter: ImageFilter.blur(
                 sigmaX: 5,
                 sigmaY: 5,
               ),
-              child: image,
+              child: widget.image,
             )
-          : image,
+          : widget.image,
     );
   }
 }
 
-//  Container(
-//       height: height,
-//       width: width,
-//       decoration: BoxDecoration(
-//         borderRadius: BorderRadius.circular(SimpleConstants.borderRadius),
-//         image: DecorationImage(
-//           fit: BoxFit.cover,
-//           image: image.image,
-//           onError: (exception, stackTrace) {
-//             debugPrint('Error loading image: $exception');
+
+// class SimpleImage extends StatelessWidget {
+//   SimpleImage(
+//     String imageUrl, {
+//     super.key,
+//     this.fit = BoxFit.cover,
+//     this.height,
+//     this.width,
+//     this.borderRadius = SimpleConstants.borderRadius,
+//     this.blurred = false,
+//     this.child,
+//   }) : image = Image.network(
+//           imageUrl,
+//           height: height,
+//           width: width,
+//           fit: fit,
+//           errorBuilder: (context, error, stackTrace) {
+//             return const Placeholder();
 //           },
-//         ),
-//       ),
-//       child: child,
+//         );
+
+//   SimpleImage.asset(
+//     String assetPath, {
+//     super.key,
+//     this.fit = BoxFit.cover,
+//     this.height,
+//     this.width,
+//     this.borderRadius = SimpleConstants.borderRadius,
+//     this.blurred = false,
+//     this.child,
+//   }) : image = Image.asset(
+//           assetPath,
+//           height: height,
+//           width: width,
+//           fit: fit,
+//           errorBuilder: (context, error, stackTrace) {
+//             return const Placeholder();
+//           },
+//         );
+
+//   final Image image;
+//   final BoxFit fit;
+//   final double? height;
+//   final double? width;
+//   final double borderRadius;
+//   final bool blurred;
+//   final Widget? child;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ClipRRect(
+//       borderRadius: BorderRadius.circular(borderRadius),
+//       child: blurred
+//           ? ImageFiltered(
+//               imageFilter: ImageFilter.blur(
+//                 sigmaX: 5,
+//                 sigmaY: 5,
+//               ),
+//               child: image,
+//             )
+//           : image,
 //     );
+//   }
+// }
