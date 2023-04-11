07a261fd63719df70c1d3ba70eebc750761a4b18
Massimo Del Pezzo <massimo.dpezzo@gmail.com>
2023-04-11 17:46:58 +0700

    Restored mobile breakpoint at 600

--- packages/atomsbox/lib/src/atomsbox/atoms/config/app_breakpoints.dart
+++ packages/atomsbox/lib/src/atomsbox/atoms/config/app_breakpoints.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppBreakpoints {
   static bool isMobile(BuildContext context) =>
-      MediaQuery.of(context).size.width <= 720;
+      MediaQuery.of(context).size.width <= 600;
 
   static bool isTablet(BuildContext context) =>
       MediaQuery.of(context).size.width < 1295 &&
-      MediaQuery.of(context).size.width > 720;
+      MediaQuery.of(context).size.width > 600;
 
   static bool isDesktop(BuildContext context) =>
       MediaQuery.of(context).size.width >= 1295;
